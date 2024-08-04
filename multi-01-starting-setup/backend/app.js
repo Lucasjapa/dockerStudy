a525ac79e22b16386769331b6efaf145387de530
Lucas Silva <lucascdsilva.bj@gmail.com>
2024-08-03 21:53:55 -0300

    Modulo de multicontainers

---
+++ multi-01-starting-setup/backend/app.js
@@ -0,0 +1,124 @@
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const Goal = require('./models/goal');
+
+const app = express();
+
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, 'logs', 'access.log'),
+    {flags: 'a'}
+);
+
+app.use(morgan('combined', {stream: accessLogStream}));
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.get('/goals', async (req, res) => {
+    console.log('TRYING TO FETCH GOALS');
+    try {
+        const goals = await Goal.find();
+        res.status(200).json({
+            goals: goals.map((goal) => ({
+                id: goal.id,
+                text: goal.text,
+            })),
+        });
+        console.log('FETCHED GOALS');
+    } catch (err) {
+        console.error('ERROR FETCHING GOALS');
+        console.error(err.message);
+        res.status(500).json({message: 'Failed to load goals.'});
+    }
+});
+
+app.post('/goals', async (req, res) => {
+    console.log('TRYING TO STORE GOAL');
+    const goalText = req.body.text;
+
+    if (!goalText || goalText.trim().length === 0) {
+        console.log('INVALID INPUT - NO TEXT');
+        return res.status(422).json({message: 'Invalid goal text.'});
+    }
+
+    const goal = new Goal({
+        text: goalText,
+    });
+
+    try {
+        await goal.save();
+        res
+            .status(201)
+            .json({message: 'Goal saved', goal: {id: goal.id, text: goalText}});
+        console.log('STORED NEW GOAL');
+    } catch (err) {
+        console.error('ERROR FETCHING GOALS');
+        console.error(err.message);
+        res.status(500).json({message: 'Failed to save goal.'});
+    }
+});
+
+app.delete('/goals/:id', async (req, res) => {
+    console.log('TRYING TO DELETE GOAL');
+    try {
+        await Goal.deleteOne({_id: req.params.id});
+        res.status(200).json({message: 'Deleted goal!'});
+        console.log('DELETED GOAL');
+    } catch (err) {
+        console.error('ERROR FETCHING GOALS');
+        console.error(err.message);
+        res.status(500).json({message: 'Failed to delete goal.'});
+    }
+});
+
+/**
+ * `host.docker.internal` é um nome de host utilizado para acessar o host da máquina a partir de um contêiner Docker.
+ *
+ * Por padrão, `host.docker.internal` funciona no Docker Desktop para Windows e macOS. No entanto, no Linux, esse nome de host
+ * não é suportado nativamente e não está disponível. Para Linux, é necessário utilizar alternativas, como o IP do gateway Docker
+ * (`172.17.0.1`) ou configurar uma rede Docker personalizada.
+ *
+ * Note que a funcionalidade do `host.docker.internal` pode variar dependendo da configuração e versão do Docker.
+ */
+// const {MongoClient} = require('mongodb');
+// const url = 'mongodb://172.17.0.1:27017/course-goals';
+// const client = new MongoClient(url, {useUnifiedTopology: true});
+//
+// client.connect(err => {
+//     if (err) {
+//         console.error('Failed to connect to the database', err);
+//     } else {
+//         console.log('Connected successfully to the database');
+//         app.listen(80);
+//     }
+//     // client.close();
+// });
+
+mongoose.connect(
+    'mongodb://mongodb:27017/course-goals',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err) => {
+        if (err) {
+            console.error('FAILED TO CONNECT TO MONGODB');
+            console.error(err);
+        } else {
+            console.log('CONNECTED TO MONGODB');
+            app.listen(80);
+        }
+    }
+);
